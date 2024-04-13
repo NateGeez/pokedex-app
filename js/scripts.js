76af13a87ab5f34cfe837ce2a24ed7e0451d7c79
NateGeez <160933873+NateGeez@users.noreply.github.com>
2024-04-13 08:34:49 -0500

    added p tag to for loop
    also put largest pokemon at top of array (instead of last) to make sure height alert was inserted correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
-let pokemonList=[
+let pokemonList = [
+    {name: "beartic", height: "10", type: "ice"},
     {name: "balbasure", height: "7", type: "grass"},
-    {name: "eve", height: "5", type: ["water", "speed"]},
-    {name: "beartic", height: "10", type: "ice"}
+    {name: "eve", height: "5", type: ["water", "speed"]}
+
 ]
 
 for (let i=0; i < pokemonList.length; i++){
-    document.write (pokemonList[i].name + " (height: "
-    + pokemonList[i].height + ") ");
+    document.write ("<p>" + pokemonList[i].name + " (height: "
+    + pokemonList[i].height + ") " + "</p>");
     if  (pokemonList[i].height > 8)
     document.write ("Wow, that's big!");
 }
