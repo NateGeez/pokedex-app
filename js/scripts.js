a619a4d84a929574d978985a239668989fc08b55
NateGeez <160933873+NateGeez@users.noreply.github.com>
2024-04-20 15:51:11 -0500

    wrapped my array in IIFE and restructured forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,32 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+    
+    let pokemonList = [
     {name: "beartic", height: "10", type: "ice"},
     {name: "balbasure", height: "7", type: "grass"},
     {name: "eve", height: "5", type: ["water", "speed"]}
+    ]
 
-]
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
 
-function myLoopFunction(pokemon) {
-    document.write("</br>" + pokemon.name + " (height: " 
-    + pokemon.height + ") ");
-    if (pokemon.height > 8)
-    document.write ("Wow, that's big!");
-}
-pokemonList.forEach(myLoopFunction);
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+// forEach loop
+pokemonList.forEach(function (pokemon) {
+    if (pokemon.height >= 8) {
+        document.write("</br>" + pokemon.name + "(height: " 
+        + pokemon.height + ") Wow, that's big!");
+    } else if (pokemon.height) {
+        document.write("</br>" + pokemon.name + "(height: " 
+        + pokemon.height + ")")
+    }
+});
